d2b82e3b68fa0d5fd54e4833950aba160014b346
Dan Steinman <dansteinman@gmail.com>
2019-03-16 21:12:02 -0400

    add published deps, add client build to /docs

---
+++ docs/precache-manifest.c4192a8f6fd12636c692619e04208b31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "cfaa8a437206ef9a77f2",
+    "url": "/static/js/main.cfaa8a43.chunk.js"
+  },
+  {
+    "revision": "2813801d476a5ec4fdf7",
+    "url": "/static/js/2.2813801d.chunk.js"
+  },
+  {
+    "revision": "cfaa8a437206ef9a77f2",
+    "url": "/static/css/main.866a8263.chunk.css"
+  },
+  {
+    "revision": "53bc573a19a605615369e71e4ca3f1c5",
+    "url": "/index.html"
+  }
+];
